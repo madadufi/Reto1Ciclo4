5b102c023c983f2bfce6a58b4ba82b62ed7072d7
Madadufi <madadufi@hotmail.com>
2021-12-19 12:08:27 -0500

    initial commit from reto1

---
+++ src/main/java/com/cuatroa/retouno/repository/crud/UserCrudRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.cuatroa.retouno.repository.crud;
+
+import com.cuatroa.retouno.model.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * * @author madadufi
+ */
+public interface UserCrudRepository extends CrudRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
